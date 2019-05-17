0bcca76aa0e9397316b1c8c04337afbeffbc4e70
Shaady Dawood <shady.youssef@gmail.com>
2019-05-17 20:23:08 +0200

    test: 💍 gets/sets survive the RxStreams, and we have sent atom

--- src/core/unknown.js
+++ src/core/unknown.js
@@ -1,6 +1,8 @@
 const {BehaviorSubject, ReplaySubject, Subject} = require('rxjs');
 const {get, set, apply} = require('json-atom');
 
+let clicks = 0;
+
 const Writer = subject => {
     const startValue = {type: 'SnapShot', payload: subject.value};
     let ___init___ = true;
@@ -31,20 +33,36 @@ const show = (...args) => console.log('DEBUG::', aLine(), ...args.map(arg => typ
 const aLine = (length = 5) => (a = [], a.length = 10, a.fill('-')).join('');
 
 const journal = new BehaviorSubject({});
-journal.subscribe(entry => show('+JournalEntry', entry));
-show(journal.value);
+journal.subscribe(entry => show('+JournalEntry', ++clicks, entry, entry.gets ? entry.gets('$.hello') : 'NO .gets'));
+show('JOURNAL-VALUE:', journal.value);
 show(Writer(journal).gets('$.hello'));
 show(get('$.hello')(journal.value));
 const returns = Writer(journal).sets('$.hello')('RP');
 show({returns});
-show(journal.value);
+show('JOURNAL-VALUE:', journal.value);
 show(Writer(journal).gets('$.hello'));
 show(get('$.hello')(journal.value));
-show(journal.value);
+show('JOURNAL-VALUE:', journal.value);
 show(aLine(100));
 show(aLine(100));
 show(Writer(journal)
 .sets('$.bye')('Bad Code')
 // .get('bye')
 );
-show(journal.value);
+show('JOURNAL-VALUE:', journal.value);
+journal.value.sets('$.journaledPost')({"timestamp":"2019-05-17T18:11:29.344Z","author":"Shady Dawood"});
+
+const atom = journal.value;
+show(atom.gets('$.hello'));
+show(atom.gets('$.bye'));
+show(atom.gets('$.journaledPost'));
+show('JOURNAL-VALUE:', journal.value);
+show(atom.sets('$.hello')('New World'));
+show(atom.sets('$.bye')('Old World'));
+show(atom.sets('$.journaledPost')({"timestamp":"2019-05-17T18:14:50.344Z","author":"Shady Dawood"}));
+show('JOURNAL-VALUE:', journal.value);
+show(atom.gets('$.hello'));
+show(atom.gets('$.bye'));
+show(atom.gets('$.journaledPost'));
+show('JOURNAL-VALUE:', journal.value);
+
